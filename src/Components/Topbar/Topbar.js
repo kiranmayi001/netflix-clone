8d75ad098c7ec5986700fcec58ac280117af77e8
kiranmayi001 <kiranmayi.raj29@gmail.com>
2021-05-26 15:41:11 +0530

    first commit

---
+++ src/Components/Topbar/Topbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Topbar.css'
+
+export default function Topbar() {
+    return (
+        <div className="nav">
+            <img className = "nav__logo"
+                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+                     alt="Netflix"/>
+
+<img className = "nav__avtar"
+                src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+                alt="Netflix Logo"
+            />
+        </div>
+    )
+}
