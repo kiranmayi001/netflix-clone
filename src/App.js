c2d2596e2d71a9c3ef0d3b70049cdcef65e15bc6
kiranmayi001 <kiranmayi.raj29@gmail.com>
2021-06-03 12:16:23 +0530

    added styles

--- src/App.js
+++ src/App.js
@@ -15,6 +15,12 @@ function App() {
      isLargeRow={true}
      title="NETFLIX ORIGINAL"
      fetchUrl={requests.fetchNetFlixOriginals}/>
+       <RowCard title = "Trending Now" fetchUrl = {requests.fetchTreding}/>
+        <RowCard title = "Top Rated" fetchUrl = {requests.fetchTopRated}/>
+        <RowCard title = "Action Movies" fetchUrl = {requests.fetchActionMovies}/>
+        <RowCard title = "Horror Movies" fetchUrl = {requests.fetchHorroMovies}/>
+        <RowCard title = "Romance Movies" fetchUrl = {requests.fetchRomanticMovies}/>
+        <RowCard title = "Documentaries" fetchUrl = {requests.fetchDocumentaries}/>
     </div>
   );
 }
--- src/Components/RowCard/RowCard.js
+++ src/Components/RowCard/RowCard.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import axios from 'axios'
+import './RowCard.css'
 
 function RowCard({title,isLargeRow,fetchUrl}) {
     const [movies,setMovies] =useState([])
@@ -8,7 +9,7 @@ function RowCard({title,isLargeRow,fetchUrl}) {
 
 useEffect(() => {
   async function fetchData(){
-      const request = await axios.get("https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213")
+      const request = await axios.get(fetchUrl)
         setMovies(request.data.results)   
         console.log(request.data.results)   
         return request;    
@@ -18,10 +19,10 @@ useEffect(() => {
     return (
         <div className="row">
             {title}
-<div className="row_poster">
+<div className="row_posters">
     {movies && movies.map(movie=>{
        return <img src= {`${baseURL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt="originals"
-         className = {isLargeRow ? 'row_poster' : 'row_poster_large'}
+         className = {isLargeRow ? 'row_poster row_poster_large' : 'row_poster'}
         />
 
     })}
@@ -31,3 +32,7 @@ useEffect(() => {
 }
 
 export default RowCard
+
+
+
+// "https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213"
